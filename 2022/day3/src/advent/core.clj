e563847fea32c29e044f957d5a0c8e8f52cfccdd
Jonathan Lundholm <jonathal@axis.com>
2022-12-03 23:11:44 +0100

    Add solution for 2022, day 3

---
+++ 2022/day3/src/advent/core.clj
@@ -0,0 +1,66 @@
+(ns advent.core
+  (:gen-class) 
+  (:require
+    [clojure.java.io :as io]
+    [clojure.string :as str]
+    [clojure.set :as set]))
+
+(def scores "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+(defn score [ch]
+  (+ 1 (str/index-of scores ch)))
+
+(defn half
+  "Splits line in half"
+  [line]
+  (let [len (/ (count line) 2)
+        a (take len line)
+        b (take-last len line)]
+    [a b]))
+
+(defn priority [line]
+  (let [[a b] (half line)
+        common (set/intersection (set a) (set b))
+        ch (first common)]
+    (score ch)))
+
+(defn group
+  "Group list of lines into groups of three."
+  ([list groups curr]
+    (if (empty? list)
+      (conj groups curr)
+      (let [head (first list)
+            tail (rest list)]
+        (case (count curr)
+          3 (group tail (conj groups curr) [head])
+          (group tail groups (conj curr head))))))
+  ([list] (group list [] [])))
+
+(defn group-intersect
+  [group]
+  (let [[a b c] group
+        a (set a)
+        b (set b)
+        c (set c)]
+    (set/intersection a b c)))
+
+(defn readLines
+  [filepath]
+  (with-open [reader (io/reader filepath)]
+    (reduce conj [] (line-seq reader))))
+
+(defn run
+  [lines]
+  (let [priorities (map priority lines)
+        groups (group lines)
+        ; groups (map str/join groups)]
+        groups (map group-intersect groups)
+        groups (map #(score (first %)) groups)]
+    (println (reduce + priorities)) ; part 1
+    (println (reduce + groups)) ; part 2
+))
+
+(defn -main
+  ([]
+    (println "must provide input file as first argument"))
+  ([infile]
+    (run (readLines infile))))
