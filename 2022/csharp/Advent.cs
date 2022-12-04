776ee420188bb01d7dbb751ac77fba7aab216702
Jonathan Lundholm <jonathal@axis.com>
2022-12-04 21:44:21 +0100

    Add C# template

---
+++ 2022/csharp/Advent.cs
@@ -0,0 +1,15 @@
+namespace Advent
+{
+    public class Solution
+    {
+        private string[] lines;
+
+        public Solution(string[] lines)
+        {
+            this.lines = lines;
+        }
+
+        public int Part1() => 1;
+        public int Part2() => 2;
+    }
+}
---
+++ 2022/csharp/Program.cs
@@ -0,0 +1,21 @@
+// See https://aka.ms/new-console-template for more information
+
+using Advent;
+
+if (args.Length != 1)
+{
+    Console.WriteLine("must provide input file as argument");
+    Environment.Exit(1);
+}
+
+var lines = new string[] { };
+foreach (string line in File.ReadLines(args.First()))
+{
+
+    lines.Append(line);
+}
+
+var solution = new Solution(lines);
+
+Console.WriteLine($"Part 1: {solution.Part1()}");
+Console.WriteLine($"Part 2: {solution.Part2()}");
